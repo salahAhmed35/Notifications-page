0b85f31acdfe5ca772a5b6a03c6abf5fef9894f5
unknown <salahkobia13@gmail.com>
2023-02-18 12:05:37 +0200

    created notifications page

---
+++ js/notifications page.js
@@ -0,0 +1,16 @@
+let notRead = document.querySelectorAll(".notifications .not-read");
+let notificationsNumber= document.querySelector(".notifications-number");
+let makeAllRead = document.querySelector(".all-read");
+
+
+
+
+notificationsNumber.innerHTML = notRead.length
+
+
+    makeAllRead.onclick = function(){
+        notRead.forEach((e) => {
+            e.classList.remove("not-read")
+        })
+        notificationsNumber.innerHTML = `0`
+    }
